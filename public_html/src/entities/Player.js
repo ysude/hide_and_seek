2e968de76e55be1905d713ce4cf54d2b68086317
ysude <sudeyldrm04@hotmail.com>
2026-01-14 12:11:41 +0300

    Configure Git LFS tracking

--- public_html/src/entities/Player.js
+++ public_html/src/entities/Player.js
@@ -4,3 +4,23 @@
  */
 
 
+// entities/Player.js
+import * as THREE from "three";
+
+export class Player {
+    constructor(camera, controls) {
+        this.camera = camera;
+        this.controls = controls;
+        
+        // Oyuncu Özellikleri
+        this.health = 100;
+        this.inventory = [];
+        this.height = 1.7; // Göz hizası
+        this.speed = 4;
+        this.runSpeed = 8;
+    }
+
+    update(dt, keys) {
+        // Hareket mantığını Game.js'den buraya taşıyabilirsin
+    }
+}
